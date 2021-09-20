e7fd1d9f6ddb333cf9c690f1d176a45eabac3e40
gokul1027 <gokul1027@gmail.com>
2021-09-20 19:15:54 +0530

    Update Demo.java

--- src/test/Demo.java
+++ src/test/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
-			System.out.println("Hello Chitti");
+			System.out.println("Hello Chitti gokul");
 		// TODO Auto-generated method stub
 
 	}
